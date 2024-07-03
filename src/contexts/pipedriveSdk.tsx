2ce4b2c0cdbca691efbd1bc1169f1938e1dc74c8
Remy Johnny <ewerejohnnyjnr93@gmail.com>
2024-07-03 16:54:22 +0300

    removed explitly id for sdk

--- src/contexts/pipedriveSdk.tsx
+++ src/contexts/pipedriveSdk.tsx
@@ -15,9 +15,7 @@ export default function SdkContextProvider({
   useEffect(() => {
     const initializeSDK = async () => {
       try {
-        const sdk = await new AppExtensionsSDK({
-          identifier: "e85f2d42-792c-4697-a25c-c42b242eb1d8",
-        }).initialize({ size: { height: 500 } });
+        const sdk = await new AppExtensionsSDK().initialize();
         setSdk(sdk);
       } catch (error) {
         console.log(error);
